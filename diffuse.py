a6ea461e72d7faa8d7464f49cdbcd165c9e24945
fenghe <jxphxufh@gmail.com>
2019-03-01 21:14:12 +0800

    add diffuse

---
+++ diffuse.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+	GPIO.setmode(GPIO.BCM)
+
+	GPIO.setup(18, GPIO.IN)
+
+try:
+	while True:
+		data = GPIO.input(18)
+
+except KeyboardInterrupt: 
+	GPIO.cleanup()

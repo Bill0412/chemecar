0ac1fabf76e402c25fa5d6eaaed8191d6c5dad83
fenghe <jxphxufh@gmail.com>
2019-05-02 15:54:46 +0800

    refactor: split the project to 3(1 + 2) modules

--- solution-cv/motor.py
+++ solution-cv/motor.py
@@ -1,18 +1,21 @@
+## motor.py
+## class: Motor
+# This module is used to control the motor
+# by using a 5V voltage controled trigger 
+# connected to the Raspberry Pi
+
 from RPi.GPIO import *
 
-setmode(BCM)
+class Motor:
+	def __init__(self):
+		setmode(BCM)
 
-Trigger = 15
-# control the trigger
-setup(Trigger, OUT)
+		self.trigger = 15
 
-try:
-    while True:
-        output(Trigger, LOW)
-except:
-    cleanup()
-    
-    
+		setup(self.trigger, OUT)
 
+	def move(self):
+		output(self.trigger, HIGH)
 
-
+	def stop(self):
+		output(self.trigger, LOW)
